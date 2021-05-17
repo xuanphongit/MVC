3a3dda821727fefe6832d96556e26d668ee61772
PhongMX <xuanphongit1109@gmail.com>
2021-05-17 15:52:34 +0700

    Remove unused lib

---
+++ PhongShop.Model/Models/Footer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhongShop.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        public string ID { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
